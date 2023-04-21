5c361eb10904ad86b8c6e0503a0132d519205f20
AGoetzee <arthurgoetzee@outlook.com>
2023-04-21 18:21:25 +0200

    added parsing function

--- prepare_labels.py
+++ prepare_labels.py
@@ -9,9 +9,9 @@ os.chdir(SCICORE_PDB_LOC)
 
 data = pd.read_csv('data_preprocessed.csv')
 
-open
+f = open('test.txt','w')
 
-for pdb_id, chain in data[['PDB_ID','Chains']].head(100).to_numpy():
+for pdb_id, chain, labels in data[['PDB_ID','Chains', 'res_string']].head(100).to_numpy():
 
     # Find the right folder
     loc = f'{pdb_id[2,3]}/pdb{pdb_id}.ent.gz'
@@ -20,4 +20,10 @@ for pdb_id, chain in data[['PDB_ID','Chains']].head(100).to_numpy():
     ent = io.loadPDB(loc)
 
     # Make a selection
-    
+    selection = ent.Select(f'chain={chain} and protein=true')
+
+    # Write the PDB identifier in the file
+    f.write(f'>{pdb_id}_{chain}')
+
+    for index, residue, label in (selection.index, selection.residues, labels):
+        f.write(f'{chain} {index} {residue} {label}')
